50bb80f548be7ad46833a4c3d2da124f9b94f3f4
JonnyTischbein <jonny_tischbein@systemli.org>
2019-11-06 09:17:57 +0100

    current app changes

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,15 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190815111147) do
+ActiveRecord::Schema.define(version: 20190823095045) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "workday_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "workdays", force: :cascade do |t|
     t.datetime "start_time"
     t.datetime "end_time"
-    t.string   "category"
     t.string   "comment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
 
 end
